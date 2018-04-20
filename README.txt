eff7bf4554936dfadc448be30cd85d6b8cedacfa
feyr <feyr@oregonstate.edu>
2018-04-20 11:15:20 -0700

    version immediately prior to running bbduk to trim primer and vector sequences

--- README.txt
+++ README.txt
@@ -4,9 +4,13 @@
 # Goal: allow further processing on these files
 
 # step 1: create reference fasta file containing primer and vector sequences to remove from reads
+# primers_SangerSeq.fasta
+# pCR4_vectorSeq.fasta
 
 # step 2: write bbduk command to remove these sequences
+#runBBDuk.sh
 
 # step 3: run bbduk
+SGE_Batch -c 'runBBDuk.sh' -r logBBDukTrim
 
 # step 4: check efficacy of primer and vector sequence removal
--- runBBDuk.sh
+++ runBBDuk.sh
@@ -1,8 +1,8 @@
-for f in /ACTF/Course/mb599_bds_s18/data/share/group1_Ream/IBDfasta/*; do infile=$(basename -- "$f"); file="${infile%.*}"; outfile="${file}.trimmed.fasta"; bbduk.sh in=$infile out=$outfile ref=pCR4_vectorSeq.fasta stats=sangerStats_vectorR.txt rcomp=t ktrim=r mink=11 k=31 hdist=1; done
+for f in /ACTF/Course/mb599_bds_s18/data/share/group1_Ream/IBDfasta/*; do infile=$(basename -- "$f"); file="${infile%.*}"; outfile="${file}.trimmedR.fasta"; bbduk.sh in=$infile out=$outfile ref=pCR4_vectorSeq.fasta stats=sangerStats_vectorR.txt rcomp=t ktrim=r mink=11 k=31 hdist=1; done
 
-for f in /ACTF/Course/mb599_bds_s18/data/share/group1_Ream/IBDfasta/*; do infile=$(basename -- "$f"); file="${infile%.*}"; outfile="${file}.trimmed.fasta"; bbduk.sh in=$infile out=$outfile ref=pCR4_vectorSeq.fasta stats=sangerStats_vectorL.txt rcomp=t ktrim=l mink=11 k=31 hdist=1; done
+for f in /ACTF/Course/mb599_bds_s18/home/feyr/IBDteam1/bbdukTrim/*.trimmedR.fasta; do infile=$(basename -- "$f"); file="${infile%.trimmedR.fasta}"; outfile="${file}.trimmedRL.fasta"; bbduk.sh in=$infile out=$outfile ref=pCR4_vectorSeq.fasta stats=sangerStats_vectorL.txt rcomp=t ktrim=l mink=11 k=31 hdist=1; done
 
-for f in /ACTF/Course/mb599_bds_s18/data/share/group1_Ream/IBDfasta/*; do infile=$(basename -- "$f"); file="${infile%.*}"; outfile="${file}.trimmed.fasta"; bbduk.sh in=$infile out=$outfile ref=primers_SangerSeq.fasta stats=sangerStats_primers.txt rcomp=t ktrim=r mink=11 k=20 hdist=1; done
+for f in /ACTF/Course/mb599_bds_s18/home/feyr/IBDteam1/bbdukTrim/*.trimmedRL.fasta; do infile=$(basename -- "$f"); file="${infile%.trimmedRL.fasta}"; outfile="${file}.trimmedRLP.fasta"; bbduk.sh in=$infile out=$outfile ref=primers_SangerSeq.fasta stats=sangerStats_primers.txt rcomp=t ktrim=r mink=11 k=20 hdist=1; done
 
 # remove vector sequences on either side of insertion site (range 11 to 31 nt) from 3' end of reads
 # bbduk.sh in=$infile out=$outfile ref=pCR4_vectorSeq.fasta stats=sangerStats_vectorR.txt rcomp=t ktrim=r mink=11 k=31 hdist=1
